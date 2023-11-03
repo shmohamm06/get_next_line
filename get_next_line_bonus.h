78dcb77744ea6ed00e5fd032ebf15821de640cbe
Shahriar Shadman Mohammad <138768347+shmohamm06@users.noreply.github.com>
2023-11-03 23:14:45 +0400

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shmohamm <shmohamm@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/06 12:35:29 by shmohamm          #+#    #+#             */
+/*   Updated: 2023/08/06 12:36:00 by shmohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*line_by_line(char *s);
+char	*del_line(char *s);
+char	*read_line(char *str, int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
